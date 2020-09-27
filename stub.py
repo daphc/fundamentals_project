c14c86de779b666d45e912fdf10f69f58ed5acd9
Daphne <70164262+daphc@users.noreply.github.com>
2020-09-26 20:56:44 -0700

    established basics of extracting data with web services and pandas, although molecularWeight query isn't giving expected values

--- stub.py
+++ stub.py
@@ -1,10 +1,28 @@
-#this is a VERY rough draft of a potential outline for our project.
-import requests
+import pandas
 import matplotlib.pyplot as mpl
 import numpy as np
 
-def collect_by_year(filename): #input: a CSV file from RCSB web services
+def collect_by_year():
     """ get ids of structures that were released in a particular year """
+    #all PDBS: data = pandas.read_csv("https://www.rcsb.org/pdb/rest/customReport.csv?pdbids=*&customReportColumns=structureId,releaseDate,molecularWeight&format=csv&service=wsfile")
+    years_ids = {} #keys are the years. values are lists of pdb ids
+    #test link:
+    data = pandas.read_csv("https://www.rcsb.org/pdb/rest/customReport.csv?pdbids=100D,4HYA,3IRL&customReportColumns=structureId,releaseDate,molecularWeight&format=csv&service=wsfile")
+    print(data.head())
+    #Investigate NaN displayed -- maybe we need to ask for a diffferent property, not molecularWeight
+    for index, row in data.iterrows():
+         if row[2] not in years_ids.keys(): #year not in dictionary
+            print("new year and PDB ID added to dictionary.")
+            years_ids[row[2]] = [row[0]]
+         elif row[2] in years_ids.keys() and row[0] not in years_ids[row[2]]: #year in dictionary but a pdb id from that year isn't
+            print("new PDB ID added to already existing year.")
+            years_ids[row[2]].append(row[0])
+         elif row[2] in years_ids.keys() and row[0] in years_ids[row[2]]:
+            print("Duplicate skipped.")
+         else:
+            print("Something went wrong.")
+        # need to incorporate error handling for NaN molecular weight.
+    print(years_ids)
     #append to years_ids.
 
 def get_avg_mw(dates_files):
@@ -12,15 +30,17 @@ def get_avg_mw(dates_files):
     #from years_ids, extract the average molecular weight and append to years_mw.
 
 def get_num_struct(dates_files):
-    """ get the number of released structures from a particular year """"
+    """ get the number of released structures from a particular year """
     #from years_ids, get the number of structures and append to years_num.
 
 def main():
-    """ gather the relevant quantities to make the plot """
-    years_ids = {} #keys are the years. values are lists of pdb ids
+    """ gather the relevant quantities and make the plot """
     years_mw = {} #keys are the years. values are the avg molecular weight from each year.
     years_num = {} #keys are the years. values are the number of structures from each year.
-    collect_by_year(csv_file)
-    get_avg_mw(years_ids)
-    get_num_struct(years_ids)
+    collect_by_year()
+    # get_avg_mw(years_ids)
+    # get_num_struct(years_ids)
     #plotting: x values are dictionary keys, y values are key values
+
+if __name__ == "__main__":
+    main()
