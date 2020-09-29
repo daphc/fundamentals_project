bc1f34ae298564bfb8524e3420cccdd904357ec1
Daphne <70164262+daphc@users.noreply.github.com>
2020-09-29 14:39:04 -0700

    plotting and error handling

--- please_go_fast_stub.py
+++ please_go_fast_stub.py
@@ -1,6 +1,8 @@
 import pandas
+import numpy as np
 import matplotlib.pyplot as mpl
 from statistics import mean
+import urllib
 
 def get_avg_mw(dataframe):
     """ get the average molecular weight from structures released in a particular year """
@@ -66,14 +68,33 @@ def get_num_struct(dataframe):
 
 def main():
     """ gather the relevant quantities and make the plot """
-    # data = pandas.read_csv("http://www.rcsb.org/pdb/rest/customReport.csv?pdbids=1stp,2jef,1cdg&customReportColumns=structureId,releaseDate,structureMolecularWeight,macromoleculeType&service=wsfile&format=csv")
-    data = pandas.read_csv("http://www.rcsb.org/pdb/rest/customReport.csv?pdbids=*&customReportColumns=structureId,releaseDate,structureMolecularWeight,macromoleculeType&service=wsfile&format=csv")
-    final_data = data.dropna().reset_index(drop=True)
-    # average_weight = get_avg_mw(final_data)
-    # number_of_strucuters = get_num_struct(final_data)
-    print ('This is the average weight\n', get_avg_mw(final_data))
-    print ('This is the number of strucuters\n', get_num_struct(final_data))
-    #plotting: x values are dictionary keys, y values are key values
+    try:
+        data = pandas.read_csv("http://www.rcsb.org/pdb/rest/customReport.csv?pdbids=*&customReportColumns=structureId,releaseDate,structureMolecularWeight,macromoleculeType&service=wsfile&format=csv")
+        final_data = data.dropna().reset_index(drop=True)
+        average_weight = get_avg_mw(final_data)
+        number_of_structures = get_num_struct(final_data)
+        plotting_data = pandas.concat([average_weight, number_of_structures[1]], axis=1 )
+        plotting_data.columns = ["Year", "Average molecular weight", "Number of structures"]
+        print("Average weight\n", average_weight)
+        print("Number of structures\n", number_of_structures)
+        plotting_data.sort_values(by="Year", axis=0, inplace=True)
+        print("This is the dataframe for plotting\n", plotting_data)
+        fig = mpl.figure()
+        ax1 = fig.add_subplot(111)
+        ax2 = ax1.twinx()
+        width=0.3
+        plotting_data.plot(x = "Year", y = "Average molecular weight", kind = "bar", ax = ax1, position=1, color="red", width=width, label="Average molecular weight")
+        plotting_data.plot(x = "Year", y = "Number of structures", kind = "bar", ax = ax2, position=0, color="blue", width=width, label="Number of structures")
+        ax1.set_title("Number of structures deposited and average molecular weight by year \n")
+        ax1.set_ylabel("Average molecular weight")
+        ax1.set_ylim(0,plotting_data.max(axis=0)["Average molecular weight"]+20000)
+        ax1.legend(loc="upper left")
+        ax2.set_ylabel("Number of structures")
+        ax2.set_ylim(0,plotting_data.max(axis=0)["Number of structures"]+2000)
+        mpl.show()
+    except urllib.error.URLError:
+        print("Please make sure you are connected to the Internet.")
+        exit()
 
 if __name__ == "__main__":
     main()
